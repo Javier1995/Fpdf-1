3d5f5883116f8876c089487d1f9d94596c6b8a7d
Javier Quiroga Almeida <quiroga218@gmail.com>
2021-10-22 09:14:29 -0500

    Se ha agregado cambios con respecto a escribir en HTML

--- original/fpdf.php
+++ original/fpdf.php
@@ -67,6 +67,16 @@ protected $LayoutMode;         // layout display mode
 protected $metadata;           // document properties
 protected $PDFVersion;         // PDF version number
 
+/* write html */
+protected $B;
+protected $I;
+protected $U;
+protected $HREF;
+protected $fontList;
+protected $issetfont;
+protected $issetcolor;
+
+
 /*******************************************************************************
 *                               Public methods                                 *
 *******************************************************************************/
@@ -101,6 +111,17 @@ function __construct($orientation='P', $unit='mm', $size='A4')
 	$this->ColorFlag = false;
 	$this->WithAlpha = false;
 	$this->ws = 0;
+
+	/* HTML ATTRIBUTES */
+
+	$this->B=0;
+    $this->I=0;
+    $this->U=0;
+    $this->HREF='';
+    $this->fontlist=array('arial', 'times', 'courier', 'helvetica', 'symbol', 'Calibri-Light');
+    $this->issetfont=false;
+    $this->issetcolor=false;
+
 	// Font path
 	if(defined('FPDF_FONTPATH'))
 	{
@@ -168,6 +189,175 @@ function __construct($orientation='P', $unit='mm', $size='A4')
 	// Set default PDF version number
 	$this->PDFVersion = '1.3';
 }
+
+
+/* html things start */
+
+
+
+protected function hex2dec($couleur = "#000000"){
+	$R = substr($couleur, 1, 2);
+	$rouge = hexdec($R);
+	$V = substr($couleur, 3, 2);
+	$vert = hexdec($V);
+	$B = substr($couleur, 5, 2);
+	$bleu = hexdec($B);
+	$tbl_couleur = array();
+	$tbl_couleur['R']=$rouge;
+	$tbl_couleur['V']=$vert;
+	$tbl_couleur['B']=$bleu;
+	return $tbl_couleur;
+}
+
+
+
+
+//conversion pixel -> millimeter at 72 dpi
+protected function px2mm($px){
+	return $px*25.4/72;
+}
+/* Conversation */
+protected  function txtentities($html){
+	$trans = get_html_translation_table(HTML_ENTITIES);
+	$trans = array_flip($trans);
+	return strtr($html, $trans);
+}
+
+
+public function WriteHTML($html, $h)
+{
+    //HTML parser
+    $html=strip_tags($html,"<b><u><i><a><img><p><br><strong><em><font><tr><blockquote>"); //supprime tous les tags sauf ceux reconnus
+    $html=str_replace("\n",' ',$html); //remplace retour à la ligne par un espace
+    $a=preg_split('/<(.*)>/U',$html,-1,PREG_SPLIT_DELIM_CAPTURE); //éclate la chaîne avec les balises
+    foreach($a as $i=>$e)
+    {
+        if($i%2==0)
+        {
+            //Text
+            if($this->HREF)
+                $this->PutLink($this->HREF,$e);
+            else
+                $this->Write($h,stripslashes($this->txtentities($e)));
+        }
+        else
+        {
+            //Tag
+            if($e[0]=='/')
+                $this->CloseTag(strtoupper(substr($e,1)));
+            else
+            {
+                //Extract attributes
+                $a2=explode(' ',$e);
+                $tag=strtoupper(array_shift($a2));
+                $attr=array();
+                foreach($a2 as $v)
+                {
+                    if(preg_match('/([^=]*)=["\']?([^"\']*)/',$v,$a3))
+                        $attr[strtoupper($a3[1])]=$a3[2];
+                }
+                $this->OpenTag($tag,$attr);
+            }
+        }
+    }
+}
+
+public function OpenTag($tag, $attr)
+{
+    //Opening tag
+    switch($tag){
+        case 'STRONG':
+            $this->SetStyle('B',true);
+            break;
+        case 'EM':
+            $this->SetStyle('I',true);
+            break;
+        case 'B':
+        case 'I':
+        case 'U':
+            $this->SetStyle($tag,true);
+            break;
+        case 'A':
+            $this->HREF=$attr['HREF'];
+            break;
+        case 'IMG':
+            if(isset($attr['SRC']) && (isset($attr['WIDTH']) || isset($attr['HEIGHT']))) {
+                if(!isset($attr['WIDTH']))
+                    $attr['WIDTH'] = 0;
+                if(!isset($attr['HEIGHT']))
+                    $attr['HEIGHT'] = 0;
+                $this->Image($attr['SRC'], $this->GetX(), $this->GetY(), $this->px2mm($attr['WIDTH']), $this->px2mm($attr['HEIGHT']));
+            }
+            break;
+        case 'TR':
+        case 'BLOCKQUOTE':
+        case 'BR':
+            $this->Ln(5);
+            break;
+        case 'P':
+            $this->Ln(10);
+            break;
+        case 'FONT':
+            if (isset($attr['COLOR']) && $attr['COLOR']!='') {
+                $coul=$this->hex2dec($attr['COLOR']);
+                $this->SetTextColor($coul['R'],$coul['V'],$coul['B']);
+                $this->issetcolor=true;
+            }
+            if (isset($attr['FACE']) && in_array(strtolower($attr['FACE']), $this->fontlist)) {
+                $this->SetFont(strtolower($attr['FACE']));
+                $this->issetfont=true;
+            }
+            break;
+    }
+}
+
+protected function CloseTag($tag)
+{
+    //Closing tag
+    if($tag=='STRONG')
+        $tag='B';
+    if($tag=='EM')
+        $tag='I';
+    if($tag=='B' || $tag=='I' || $tag=='U')
+        $this->SetStyle($tag,false);
+    if($tag=='A')
+        $this->HREF='';
+    if($tag=='FONT'){
+        if ($this->issetcolor==true) {
+            $this->SetTextColor(0);
+        }
+        if ($this->issetfont) {
+            $this->SetFont('arial');
+            $this->issetfont=false;
+        }
+    }
+}
+
+protected function SetStyle($tag, $enable)
+{
+    //Modify style and select corresponding font
+    $this->$tag+=($enable ? 1 : -1);
+    $style='';
+    foreach(array('B','I','U') as $s)
+    {
+        if($this->$s>0)
+            $style.=$s;
+    }
+    $this->SetFont('',$style);
+}
+
+protected function PutLink($URL, $txt)
+{
+    //Put a hyperlink
+    $this->SetTextColor(0,0,255);
+    $this->SetStyle('U',true);
+    $this->Write(5,$txt,$URL);
+    $this->SetStyle('U',false);
+    $this->SetTextColor(0);
+}
+
+
+/* html things end */
 
 function SetMargins($left, $top, $right=null)
 {
